3699c2f380fc3207d7a64239dbc886a6218c622f
Juan Carlos Galan Hernandez <jcgalanh@gmail.com>
2019-02-27 11:43:02 -0600

    Add player sprite

--- pixi_review/src/main/game.cljs
+++ pixi_review/src/main/game.cljs
@@ -4,6 +4,8 @@
             [pixi.js :as pixi]))
 
 
+(defonce app (atom nil))
+
 (defn destroy
   "Removes all transtient data between hot reloads"
   []
@@ -14,13 +16,23 @@
   []
   nil)
 
+(defn setup
+  []
+  (let [sprite (new pixi/Sprite (oget pixi/loader "resources.ship00.texture"))]
+    (ocall @app "stage.addChild" sprite)))
+
 (defn init
   "Run when the page is first time loaded, creates initial environment"
   []
-  (let [app (new pixi/Application (clj->js {:width 256
-                                            :height 256
-                                            :antialias true
-                                            :transparent false
-                                            :resolution 1}))]
+  (let [new-app (new pixi/Application (clj->js {:width 256
+                                                :height 256
+                                                :antialias true
+                                                :transparent false
+                                                :resolution 1}))]
+    (reset! app new-app)
     (log "Appending a pixi stage")
-    (ocall js/document "body.appendChild" (oget app "view"))))
+    (ocall js/document "body.appendChild" (oget @app "view"))
+    (log "Load resources")
+    (ocall pixi/loader "add" (clj->js {:name :ship00
+                                       :url "assets/2.png"}))
+    (ocall pixi/loader "load" setup)))
